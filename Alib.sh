3d0a84685148c092732e3ad6a81c7a94c5e70ada
偏右 <afcstar@163.com>
2013-03-09 00:29:13 +0800

    fix Alib.sh

--- Alib.sh
+++ Alib.sh
@@ -2,3 +2,11 @@
 git clone git@github.com:aliceui/Alib.git
 cd Alib
 rm -rf .git
+
+echo "你是支付宝的前端吗?"
+select yn in "是" "不是"; do
+    case $yn in
+        是 ) mv Makefile.alipay Makefile; break;;
+        不是 ) exit;;
+    esac
+done
