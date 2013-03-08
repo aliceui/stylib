7aed1db2ffdc72b9341d341d9e722d8ca6345bab
偏右 <afcstar@163.com>
2013-03-09 00:45:58 +0800

    fix shell

--- Alib-alipay.sh
+++ Alib-alipay.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-echo 'Downloading Aib ...'
-git clone git@github.com:aliceui/Alib.git
-cd Alib
-rm -rf .git
-mv Makefile.alipay Makefile
-git init
+
+git clone git@github.com:aliceui/Alib.git Alib-template
+rm -rf Alib-template/.git
+rm Alib-template/Makefile.alipay
+mv Alib-template/Makefile.alipay Alib-template/Makefile
--- Alib.sh
+++ Alib.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-echo 'Downloading Aib ...'
-git clone git@github.com:aliceui/Alib.git
-cd Alib
-rm -rf .git
-rm Makefile.alipay
-git init
+
+git clone git@github.com:aliceui/Alib.git Alib-template
+rm -rf Alib-template/.git
+rm Alib-template/Makefile.alipay
+git init Alib-template
